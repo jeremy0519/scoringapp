d482945c75075e81c669559454bdcc6d29ed3a75
fushantangzp201403 <fushantangzp201403@126.com>
2023-10-26 15:54:17 +0800

    update

--- src/components/EditPage.vue
+++ src/components/EditPage.vue
@@ -1,50 +1,76 @@
 <template>
   <div class="d-inline">
-    <span>当前联盟：</span><span v-bind:class="currentColor">{{leagueName}}</span>
-    <button type="button" @click="isBlue =! isBlue" class="btn btn-outline-secondary btn-sm m-3">Switch</button>
+    <span>当前联盟：</span><span v-bind:class="Settings.color">{{ Settings.league }}</span>
+    <button type="button" @click="Settings.isBlue = !Settings.isBlue"
+      class="btn btn-outline-secondary btn-sm m-3">Switch</button>
+  </div>
+  <div class="col-auto">
+    <label for="leads" class="form-label">分差：{{ Data.leadsValue }}</label>
   </div>
   <form name="leads" class="row align-items-center">
     <div class="col-auto">
-      <label for="leads" class="form-label">分差</label>
+      <button @click.prevent="Data.leadsValue -= 5" class="btn btn-outline-danger">-5</button>
     </div>
     <div class="col">
-      <input v-model="leadsValue" type="range" class="form-range" min="-50" max="50" step="5" id="leads">
+      <input v-model="Data.leadsValue" type="range" class="form-range" min="-50" max="50" step="5" id="leads">
     </div>
     <div class="col-auto">
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button @click.prevent="Data.leadsValue += 5" class="btn btn-outline-success">+5</button>
     </div>
   </form>
 
 
   <p class="mt-3">放置进度</p>
 
-  <table>
-    <tr class="d-flex">
-      <td id="11" class="bg-primary opacity-75">aaaaaaaaa</td>
-      <td id="12" class="bg-primary opacity-75">aaaaaaaaaa</td>
-      <td id="13" class="bg-primary opacity-75">aaaaaaaaaa</td>
-      <td id="14" class="bg-primary opacity-75">aaaaaaaaa</td>
-      <td id="15" class="bg-primary opacity-75">aaaaaaaaa</td>
-    </tr>
-  </table>
+  <div class="row gap-1 mb-1" style="height: 100px;">
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+  </div>
+  <div class="row gap-1 mb-1" style="height: 100px;">
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+  </div>
+  <div class="row gap-1 mb-1" style="height: 100px;">
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+  </div>
+  <div class="row gap-1 mb-1" style="height: 100px;">
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+  </div>
+  <div class="row gap-1 mb-1" style="height: 100px;">
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+    <div class="d-inline-block bg-primary col"></div>
+  </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
-const leadsValue = ref(20)
-const isBlue = ref(1)
-const currentColor = computed(()=>{
-  if (isBlue.value==1){
-    return "text-primary"
-  }else{
-    return "text-danger"
-  }
+const Settings = ref({
+  isBlue: 1,
+  color: computed(() => {
+    return Settings.value.isBlue ? "text-primary" : "text-danger"
+  }),
+  league: computed(() => {
+    return Settings.value.isBlue ? "蓝色" : "红色"
+  })
 })
-const leagueName = computed(()=>{
-  if (isBlue.value==1){
-    return "蓝色"
-  }else{
-    return "红色"
-  }
+const Data = ref({
+  leadsValue: 20
 })
 </script>
